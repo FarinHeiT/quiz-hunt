5403f7f85d405b1e5d02b358e238cf6f61338753
ilya83065 <ilyathomas8306@icloud.com>
2020-01-11 10:39:17 -0500

    db skeleton

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 class Config(object):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///\\Quiz-Hunt\\Maindb.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///Maindb.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
--- models.py
+++ models.py
@@ -1,19 +1,12 @@
 from app import db
 import re
 
-def sligfy (s):
-    p = 'r[^/w+]'
-    return re.sub(p, '-', s)
 
-class mod(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    # dont need slug right now
-    #def __init__(self, *args, **kwargs):
-        #super(mod, self).__init__(*args, **kwargs)
-        #self.make_slug()
-    #def make_slug(self):
-        #if self.titl:
-            #self.slug = sligfy(self.titl)
-    # end slug
+    username = db.Column(db.String(30))
+    password = db.Column(db.String(30))
+
+
     def __repr__(self):
-        return 'id = {},  title = {},  body = {},  slug = {},  '.format(self.id, self.titl, self.body, self.slug)
+        return ''.format(self.id)
