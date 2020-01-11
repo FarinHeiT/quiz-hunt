4408fcb3da3da341789d5f4c88da99b0dd8d4082
FarinHeiT <yariksuharik85@gmail.com>
2020-01-11 18:21:21 +0200

    General refactoring, DB recreation

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
-class Config(object):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///Maindb.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
