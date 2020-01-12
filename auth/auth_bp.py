0a8c714b4df59a36b6e8403a4ab0fd8a3e34ba45
FarinHeiT <yariksuharik85@gmail.com>
2020-01-12 15:45:39 +0200

    Registration view, buttons display, pwd hashing

--- auth/auth_bp.py
+++ auth/auth_bp.py
@@ -1,10 +1,12 @@
 import bcrypt
 from flask import Blueprint, render_template, url_for, redirect, flash
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 from flask_login import login_user, logout_user, login_required, current_user
 from models import User
+from app import db
 
 auth = Blueprint('auth', __name__, template_folder='templates', url_prefix='/auth')
+
 
 # TODO: Use Bcrypt
 
@@ -22,7 +24,6 @@ def login():
             if bcrypt.checkpw(form.password.data.encode(),
                               user.password.encode()):
                 login_user(user)
-                flash('Logged in successfully.', 'success')
                 return redirect(url_for('index'))
 
         flash('Wrong username or password', 'error')
@@ -34,3 +35,35 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = RegistrationForm()
+
+    # If POST request
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            flash('This username is taken...', 'error')
+            return redirect(url_for('auth.register'))
+
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(password.encode(), salt)
+
+        new_user = User(username=username, password=hashed)
+
+        # add the new user to the database
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+
+    return render_template('registration.html', form=form)
--- auth/forms.py
+++ auth/forms.py
@@ -1,8 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, EqualTo, InputRequired, Length
 
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
-    password = StringField('password', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+
+    password = PasswordField('New Password',
+                             validators=[InputRequired(),
+                                         EqualTo('password_again',
+                                                 message='Passwords must match!'),
+                                         Length(8, 128, message='Minimal password length: 8 characters')
+                                         ])
+
+    password_again = PasswordField('Repeat Password')
