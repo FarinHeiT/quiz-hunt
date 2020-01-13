51440229b525979ecff02eda63337b0dbccbb6e1
FarinHeiT <yariksuharik85@gmail.com>
2020-01-13 18:41:37 +0200

    Project restructuring

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -11,8 +11,8 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-#from auth.auth_bp import auth
-#app.register_blueprint(auth)
+from auth.auth_bp import auth
+app.register_blueprint(auth)
 
 from models import User
 login_manager = LoginManager()
@@ -22,3 +22,11 @@ login_manager.init_app(app)
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
+
+@app.route('/')
+def index():
+    return render_template('main.html')
+
+if __name__ == '__main__':
+    app.run()
+
