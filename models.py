52cd8a7510bf2843ec27f18c584c31e79b3e5468
FarinHeiT <yariksuharik85@gmail.com>
2020-01-15 12:04:48 +0200

    Poll description display and storage

--- models.py
+++ models.py
@@ -18,10 +18,9 @@ class Poll(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     title = db.Column(db.String(120), unique=True)
+    description = db.Column(db.Text(400))
     created_date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     questions = db.relationship('Question', backref='poll', lazy=True)
-
-
 
     def __repr__(self):
         return f'Poll: {self.title}'
