d2b99ed3253ae79edffc55f7fc05b9f16d882f2e
FarinHeiT <yariksuharik85@gmail.com>
2020-01-18 20:22:41 +0200

    Get_json helper method

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+import json
+
 from app import db
 from datetime import datetime
 from flask_login import UserMixin
@@ -11,7 +13,7 @@ class User(db.Model, UserMixin):
     completed_polls = db.relationship('CompletedPoll', backref='completed_by', lazy=True)
 
     def create_poll(self, title, description, data):
-        """ Creates new poll with given Q, Qno and answer options {'q1': (1, [a, b]} """
+        """ Creates new poll with given Q, Qno and answer options {'q1': (1, [a, b])} """
 
         new_poll = Poll(author_id=self.id,
                         title=title,
@@ -25,7 +27,7 @@ class User(db.Model, UserMixin):
             new_poll.create_question(q, q_no)
 
             # Get newly created question
-            question = Question.query.filter(Question.text==q, Question.poll_id==self.id).first()
+            question = Question.query.filter(Question.text == q, Question.poll_id == self.id).first()
             for option in answer_options:
                 question.create_ans_opt(option)
 
@@ -46,11 +48,21 @@ class Poll(db.Model):
     questions = db.relationship('Question', backref='poll', lazy=True)
 
     def create_question(self, text, question_no):
+        """ Creates question to the parent poll """
         new_question = Question(poll_id=self.id,
                                 text=text,
                                 question_no=question_no)
 
         self.questions.append(new_question)
+
+    def get_json(self):
+        """ Return JSON serialized poll """
+        data = {}
+        for question in self.questions:
+            answer_options = [(answer_option.text, answer_option.id)
+                              for answer_option in question.answer_options]
+            data[question.text] = (question.question_no, answer_options)
+        return json.dumps(data)
 
     def __repr__(self):
         return f'Poll: {self.title}'
@@ -64,6 +76,7 @@ class Question(db.Model):
     answer_options = db.relationship('AnswerOption', backref='question', lazy=True)
 
     def create_ans_opt(self, text):
+        """ Creates answer option to the parent question """
         new_ans_opt = AnswerOption(question_id=self.id,
                                    text=text)
 
@@ -110,4 +123,3 @@ class Suggestion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String(1000))
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
